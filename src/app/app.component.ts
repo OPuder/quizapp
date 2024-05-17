42f40f8b3bf71bd2272aa2a07ec7ca9985e1496b
Ole Puder <olepuder@gmail.com>
2024-05-17 14:13:34 +0200

    ole

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { RouterLink, RouterModule } from '@angular/router';
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
@@ -10,6 +10,31 @@ import { HttpClientModule } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'get started';
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    this.setDocumentLanguage();
+  }
+
+  setDocumentLanguage(): void {
+    // Überprüfen, ob der Code im Browser läuft
+    const isBrowser = isPlatformBrowser(this.platformId);
+    console.log('Ist es eine Browser-Umgebung?', isBrowser);
+
+    if (isBrowser) {
+      // Erkenne die Benutzersprache
+      const userLanguage = navigator.language || (navigator as any).userLanguage || 'de';
+
+      // Setze das lang-Attribut des html-Tags entsprechend der Benutzersprache
+      document.documentElement.lang = userLanguage;
+
+      // Ausgabe der erkannten Sprache in der Konsole
+      console.log('Erkannte Sprache:', userLanguage);
+      console.log('Aktuelles lang-Attribut:', document.documentElement.lang);
+    } else {
+      console.log('Dies ist keine Browser-Umgebung.');
+    }
+  }
 }
